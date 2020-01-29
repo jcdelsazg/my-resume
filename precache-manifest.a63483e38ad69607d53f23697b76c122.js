1e181440dc1504856da9ad2943aa2c58e8b96993
Juan Carlos Del Saz <jcdelsazg@gmail.com>
2020-01-29 18:35:09 +0100

    Updates

---
+++ precache-manifest.a63483e38ad69607d53f23697b76c122.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a085f485fe69865c0a5801e6bed8d24e",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "83cffd2a177f8ab3fb54",
+    "url": "/my-resume/static/css/main.d2abecad.chunk.css"
+  },
+  {
+    "revision": "a9fefcd69dbf5e4aab1d",
+    "url": "/my-resume/static/js/2.6c83b9fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-resume/static/js/2.6c83b9fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83cffd2a177f8ab3fb54",
+    "url": "/my-resume/static/js/main.b6dfe89e.chunk.js"
+  },
+  {
+    "revision": "a28fceb482e24d76442f",
+    "url": "/my-resume/static/js/runtime-main.f6aa34f0.js"
+  },
+  {
+    "revision": "38875c4488a418ef42cced7160279a21",
+    "url": "/my-resume/static/media/jcsaz.38875c44.png"
+  },
+  {
+    "revision": "da33983229bb45d7b2196257bb8754db",
+    "url": "/my-resume/static/media/netcore.da339832.png"
+  },
+  {
+    "revision": "0d8ed365842d251cd2bfa6e98d461d62",
+    "url": "/my-resume/static/media/nodejs.0d8ed365.png"
+  }
+]);
